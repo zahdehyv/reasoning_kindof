f01f3616cf045d2954c5291ff55d433ee367127b
JackRipper01 <francohernandezpiloto@gmail.com>
2025-01-31 13:08:43 -0500

    added one shoot learning for logic dataset

--- evaluate_gemini_one_shoot_learning_for_logic_dataset.py
+++ evaluate_gemini_one_shoot_learning_for_logic_dataset.py
@@ -52,6 +52,47 @@ def initialize_model():
             "response_mime_type": "text/plain",
         },
         system_instruction="""
+        ejemplo positivo:
+        pregunta:
+        En la Isla de los truhanes y los caballeros los habitantes A, B y C hacen las siguientes
+declaraciones:
+A: B es caballero
+B: Si A es caballero entonces C tambien lo es.
+Determine que son A, B y C. Demuestrelo formalmente.
+
+Buen razonamiento:
+
+Tenemos como premisa que en la isla de truhanes y caballeros toda persona solo tiene dos posibilidades, ser truhan o ser caballero, por tanto si eres truhan entonces no eres caballero y si eres caballero entonces no eres truhan.
+
+Tenemos tambien segun el ejercicio los siguientes planteamientos los cuales seran nuestras actuales premisas ( una premisa es como un axioma, es algo que es irrefutablemente verdad en dicho contexto ):
+Planteamiento 1 : A dice que B es caballero.
+Planteamiento 2 : B dice que Si A es caballero entonces C tambien lo es. 
+
+Un primer acercamiento a la respuesta podria ser pensar que sucederia si A fuera caballero. Por tanto asumiremos que A es caballero:
+como A dice que B es caballero en el Planteamiento 1, y tenemos que los caballeros dicen la verdad ,entonces dado que A es caballero, lo que dice es verdad.
+Por tanto tenemos como verdadero el hecho de que B es caballero y tambien tenemos que A es caballero ya que fue asumido.
+
+Ahora dado que B es caballero ,lo que dice es verdad, luego lo que dice es que " si A es caballero entonces C tambien lo es " en el Planteamiento 2,entonces al tener dicha proposicion, como tenemos que A es caballero ,podemos decir que C tambien es caballero.
+Luego llegamos a que todos son caballero A ,B y C. Pero lamentablemente no demostramos el ejercicio ya que tenemos que llegar a una contradiccion para poder demostrar realmente algo. Lo que hicimos fue asumir un conjunto de cosas y llegar a que nada se rompe, es decir no llegamos a ninguna contradiccion. 
+
+Entonces la informacion que podemos sacar de todo esto es que no debemos volver a asumir que A es caballero ya que no llegamos a ninguna contradiccion, ojo, eso no significa que A no sea realmente caballero, esto solo significa que si asumimos que A es caballero inicialmente, no llegaremos a nada contundente.
+
+Luego vamos a asumir que A es truhan ,quizas lleguemos a una contradiccion:
+
+Asumamos que A es truhan, luego como los truhanes dicen mentira , lo que dice A es mentira lo que significa que el significado contrario de lo que dice es verdad.
+A dice que B es caballero por tanto como A es truhan, entonces B no es caballero. Pero si B no es caballero entonces B es truhan ya que no puede ser ninguna otra cosa.
+Luego aplicamos la misma ideologia, lo que dice B en el Planteamiento 2 es falso o mentira, luego lo que dice es "Si A es caballero entonces C tambien lo es " es falso , pero ¿cuándo es falsa esa afirmacion? Pues es falsa si A es caballero y C no lo es, por tanto tenemos que A es caballero y C no es caballero, que es lo mismo que A es caballero y C es truhan, pero acabamos de llegar a una contradiccion porque habiamos asumido que A es truhan y ahora llegamos a que A es caballero lo cual es una contradiccion . Al llegar a una contradiccion podemos decir que lo que asumimos es falso, como lo que asumimos es A es truhan, entonces A es truhan es falso ,luego A no es truhan que es lo mismo que A es caballero.
+
+Luego llegamos a que A es caballero,¿significa que llegamoss a que A es caballero ? Pues al llegar a una contradiccion, podemos agregar lo contrario que asumimos que llego a la contradiccion a nuestras premisas las cuales siempre seran verdad en nuestro contexto.
+Por tanto tenemos ahora en nuestras premisas Planteamiento 1, Planteamiento 2 y A es caballero.
+
+Nos falta demostrar que es B y C.
+
+como A es caballero, lo que dice es verdad ,luego lo que dice es que B es caballero en el Planteamiento 1.
+Luego tenemos que B es caballero,y como lo que dice es verdad, lo que dice es que si A es caballero entonces C tambien lo es en el Planteamiento 2, luego como A es caballero , tenemos que C es caballero tambien.
+
+Luego demostramos que A es caballero, B es caballero y C es caballero.
+
         Muy Importante lo siguiente :Devuelve el resultado final en una lista de enteros donde el valor de la respuesta este entre los tags < answer > </answer > . Seleccionar multiples opciones como respuesta esta mal, solo una de las opciones es la respuesta correcta """
     )
 
